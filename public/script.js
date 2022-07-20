59e8095be8f582fab5e492e6fae255a83d43c9eb
Alexander <banan7280@gmail.com>
2022-07-20 08:53:24 +0300

    finished with all tasks

--- public/script.js
+++ public/script.js
@@ -6,11 +6,10 @@ function init() {
         { title: 'Jacket', price: 350 },
         { title: 'Shoes', price: 250 },
     ];
-    const renderGoodsItem = (title, price) => {
-        return `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
-    };
+    const renderGoodsItem = (title = "Название", price = "Цена") => `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
     const renderGoodsList = (list) => {
         let goodsList = list.map(item => renderGoodsItem(item.title, item.price));
+        goodsList = goodsList.join('');
         document.querySelector('.goods-list').innerHTML = goodsList;
     }
 
