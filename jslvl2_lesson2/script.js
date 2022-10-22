f8e4b41a19ffe867ea51e330eac220b87671c631
Alexander <banan7280@gmail.com>
2022-10-22 15:25:50 +0300

    Bucket in process

--- jslvl2_lesson2/script.js
+++ jslvl2_lesson2/script.js
@@ -2,7 +2,7 @@ class List {
     items = []
 
     constructor() {
-        let goods = this.fetchGood()
+        let goods = fetchGood()
         goods = goods.map(cur => {
             return new GoodItem(cur)
         })
@@ -10,20 +10,12 @@ class List {
         this.render()
     }
 
-    fetchGood() {
-        return [
-            { name: 'Shirt', price: 150 },
-            { name: 'Socks', price: 15 },
-            { name: 'Jacket', price: 50 },
-            { name: 'Shoes', price: 1500 }
-        ]
-    }
-
     render() {
         this.items.forEach(good => {
             good.render()
         })
     }
+
 }
 
 class GoodItem {
@@ -33,7 +25,6 @@ class GoodItem {
     constructor({ name, price }) {
         this.name = name
         this.price = price
-
     }
 
     render() {
@@ -41,23 +32,40 @@ class GoodItem {
         if (placeToRender) {
             const block = document.createElement('div')
             block.className = "goods-item";
-            block.innerHTML = `<img class ="goods-image" src="img/${this.name}.png" alt="${this.name}"><p class ="name">${this.name}</h3><p class="price">${this.price}$<button class="buy-button" type="button">Купить</button></p>`
+            block.innerHTML = `<img class ="goods-image" src="img/${this.name}.png" alt="${this.name}"><h3 class ="name">${this.name}</h3><p class="price">${this.price}$<button class="buy-button" type="button">Купить</button></p>`
             placeToRender.appendChild(block)
         }
     }
 }
 
-class Cart extends GoodItem {
-
+class Cart extends List {
+    render() {
+        const placeToRender = document.querySelector('.cart')
+        if (placeToRender) {
+            const block = document.createElement('div')
+            block.className = "cart-item";
+            block.innerHTML = `<p class ="name">${this.name}</p><p class="price">${this.price}$<button class="buy-button" type="button">Удалить</button></p>`
+            placeToRender.appendChild(block)
+        }
+    }
 }
 
 const ListInstance = new List()
+
+function fetchGood() {
+    return [
+        { name: 'Shirt', price: 150 },
+        { name: 'Socks', price: 15 },
+        { name: 'Jacket', price: 50 },
+        { name: 'Shoes', price: 1500 }
+    ]
+}
 
 function init() {
     const buyBtn = document.getElementsByClassName("buy-button");
     Array.prototype.forEach.call(buyBtn, function (el) {
         el.addEventListener('click', function () {
-            console.log('click');
+            new Cart();
         });
     })
 }
