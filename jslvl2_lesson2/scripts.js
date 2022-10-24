6ab00fe9c536226bcd8f86531363de9600553bb6
Alexander <banan7280@gmail.com>
2022-10-24 18:06:31 +0300

    NEW commit

--- jslvl2_lesson2/scripts.js
+++ jslvl2_lesson2/scripts.js
@@ -5,7 +5,8 @@ class List {
 
     constructor() {
         let goods = this.fetchGood()
-        goods = goods.map(cur => {
+        goods = goods.map((cur, index) => {
+            cur.index = index
             return new GoodItem(cur)
         })
         this.items.push(...goods)
@@ -27,36 +28,45 @@ class List {
         })
         const buyBtn = document.getElementsByClassName("buy-button");
         Array.prototype.forEach.call(buyBtn, function (el) {
-            el.addEventListener('click', function () {
-                new CartItem();
+            el.addEventListener('click', function (eventObj) {
+                new CartItem(eventObj);
             });
         })
     }
 }
 
 class GoodItem {
-    constructor({ name, price }) {
+    constructor({ name, price, index }) {
         this.name = name
         this.price = price
+        this.index = index
     }
     render() {
         const placeToRender = document.querySelector('.goods-list')
         if (placeToRender) {
             const block = document.createElement('div')
             block.className = "goods-item";
-            block.innerHTML = `<img class ="goods-image" src="img/${this.name}.png" alt="${this.name}"><h3 class ="name">${this.name}</h3><p class="price">${this.price}$<button class="buy-button" type="button">Купить</button></p>`
+            block.innerHTML = `<img class ="goods-image" src="img/${this.name}.png" alt="${this.name}"><h3 class ="name">${this.name}</h3><p class="price">${this.price}$</p><button class="buy-button" id="${this.index}" type="button">Купить</button>`
             placeToRender.appendChild(block)
         }
     }
 }
 
-class CartItem extends List {
+class CartItem {
+    constructor(eventObj) {
+        // console.log(typeof +(eventObj.target.id))
+        console.log(List.items)
+        // this.name = List.items.name[+(eventObj.target.id)]
+        // this.price = List.items.price[+(eventObj.target.id)]
+        // console.log(this.name, this.price)
+        // this.render();
+    }
     render() {
         const placeToRender = document.querySelector('.cart')
         if (placeToRender) {
             const block = document.createElement('div')
             block.className = "cart-item";
-            block.innerHTML = `<p class ="name"></p><p class="price"><button class="buy-button" type="button">Удалить</button></p>`
+            block.innerHTML = `<p class ="cartItem">${this.name} ${this.price} <button class="buy-button" type="button">Удалить</button></p>`
             placeToRender.appendChild(block)
         }
     }
